d4c0b20d5992eb56f35283b11280e9f3040b4089
Jan Weil <jan.weil@web.de>
2020-09-25 22:35:21 +0200

    Forward API requests to clair.jawebada.de

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,14 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
 import axios from 'axios'
 import { mapResourceModules } from '@reststate/vuex'
+import Vue from 'vue'
+import Vuex from 'vuex'
 
 const httpClient = axios.create({
   baseURL: '/api/inspect/v1/',
   headers: {
-    'Content-Type': 'application/vnd.api+json'
+    common: {
+      'Content-Type': 'application/vnd.api+json'
+    }
   }
 })
 
@@ -15,8 +17,8 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   modules: {
     ...mapResourceModules({
-      httpClient,
-      names: ['nodes', 'timeseries']
+      names: ['nodes', 'timeseries'],
+      httpClient
     })
   }
 })
--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,10 @@
 module.exports = {
   devServer: {
     proxy: {
-      '^/api': {
-        target: 'http://ulrichs-mbp.wxwjofiljqw2iqlh.myfritz.net:5000',
-        changeOrigin: false
-      },
-      '^/audio': {
-        target: 'http://localhost:5000',
-        changeOrigin: false
+      '/api': {
+        target: 'https://clair.jawebada.de',
+        changeOrigin: true
       }
     }
-  },
-  transpileDependencies: [
-    'vuetify'
-  ]
+  }
 }
