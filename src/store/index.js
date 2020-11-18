d6bc67b5dd94b703c956e701355a50d84feffb08
Jan Weil <jan.weil@web.de>
2020-11-18 20:49:28 +0100

    Adapt to API changes (address -> addresses)

--- src/store/index.js
+++ src/store/index.js
@@ -17,7 +17,7 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   modules: {
     Address: resourceModule({
-      name: 'address',
+      name: 'addresses',
       httpClient: httpClient
     }),
     Organization: resourceModule({
